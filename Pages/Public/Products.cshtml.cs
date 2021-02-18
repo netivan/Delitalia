eec8500614af217f596b41831e31c798c072c784
SANO-LENOVO\TeknikFix <sanogharzani@gmail.com>
2021-02-18 17:22:38 +0100

    Ekologisk Sortering

--- Pages/Public/Products.cshtml.cs
+++ Pages/Public/Products.cshtml.cs
@@ -13,8 +13,10 @@ namespace DelliItalia_Razor.Pages.Public
     public class ProductsModel : PageModel
     {
         private readonly DelliItalia_Razor.Data.DelliItalia_RazorContext _context;
+
         [BindProperty(SupportsGet =true)]
-        public string SearchProduct { get; set; }
+        public string Ekologisk { get; set; }
+
         [BindProperty(SupportsGet =true)]
         public string Category { get; set; }
 
@@ -27,14 +29,16 @@ namespace DelliItalia_Razor.Pages.Public
 
         public async Task OnGetAsync()
         {
-           
-            await GetCategory();
+                await GetCategory();
+            
+                await GetEco();
         }
       
         public async Task<List<ProductModel>> GetCategory()
         {
           if (string.IsNullOrEmpty(Category))
             {
+                
                 return ProductModel = await _context.ProductModel.ToListAsync();
             }
             else
@@ -52,8 +56,37 @@ namespace DelliItalia_Razor.Pages.Public
                     }
                     return ProductModel = _context.ProductModel.ToList();
                 }
-                catch(ArgumentOutOfRangeException) { return ProductModel = _context.ProductModel.ToList(); }
-            
+                catch(ArgumentOutOfRangeException) { return ProductModel = _context.ProductModel.ToList();
+                }
         }
+
+        public async Task<List<ProductModel>> GetEco()
+        {
+            if (string.IsNullOrEmpty(Ekologisk))
+            {
+               
+                return   await GetCategory();
+            }
+            else
+                try
+                {
+                    if (!string.IsNullOrEmpty(Ekologisk))
+                    {
+                        ProductModel = _context.ProductModel.Where(p => p.Eco==(true)).ToList();
+                        if (ProductModel.Count == 0)
+                        {
+                            ViewData["IngaProd"] = "Kategori " + Category + " har inga produkter";
+                            return ProductModel = _context.ProductModel.ToList();
+                        }
+                        return ProductModel;
+                    }
+                    return ProductModel = _context.ProductModel.ToList();
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    return ProductModel = _context.ProductModel.ToList();
+                }
+        }
+
     }
 }
