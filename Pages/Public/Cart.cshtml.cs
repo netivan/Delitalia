b0192c1e5abaa7a33e32a79893ce6e23c6fa003c
SANO-LENOVO\TeknikFix <sanogharzani@gmail.com>
2021-02-20 00:44:16 +0100

    Clean-ups search input+button inline

--- Pages/Public/Cart.cshtml.cs
+++ Pages/Public/Cart.cshtml.cs
@@ -2,12 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DelliItalia_Razor.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DelliItalia_Razor.Pages.Public
 {
-    public class CartModel : PageModel
+    public class CartModel : BaseModel
     {
         public void OnGet()
         {
