e13f425a27229089bce436f52775e6683344a1a1
SANO-LENOVO\TeknikFix <sanogharzani@gmail.com>
2021-04-09 13:58:03 +0200

    Updates

---
+++ Model/RoleModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DelliItalia_Razor.Model
+{
+    public class RoleModel
+    {
+        [Required]
+        [DisplayName("Roll/Behörighet:")]
+        public string RoleName { get; set; }
+    }
+}
